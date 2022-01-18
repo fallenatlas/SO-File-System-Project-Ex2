e8afbb062c5cc905aee5de1674ed4855a3f48a6c
fallenatlas <tiago.a.p.antunes@gmail.com>
2022-01-18 15:03:25 +0000

    Fixed test for destroy_all_after_closed with loops and added it to the Makefile.

--- tests/lib_destroy_after_all_closed_cicle_test.c
+++ tests/lib_destroy_after_all_closed_cicle_test.c
@@ -16,29 +16,36 @@
 #define SIZE 40
 
 int open_files = 0;
-int f;
 
 void *fn_thread(void *arg) {
     (void)
         arg; /* Since arg is not used, this line prevents a compiler warning */
+    int f;
+    int c;
     char buffer[SIZE];
     memset(buffer, 'A', SIZE);
     
     for (int i=0; i<10; i++){
         f = tfs_open("/f1", TFS_O_CREAT);
         if (f != -1) {
-            printf("Open file: %d\n", i);
             ++open_files;
+            //printf("Open file: %d %d\n", i, f);
         }
         else {
             printf("Failed open file: %d\n", i);
+            return NULL;
         }
         tfs_write(f, buffer, SIZE);
         /* set *before* closing the file, so that it is set before
            tfs_destroy_after_all_close returns in the main thread
         */
-        --open_files;
-        assert(tfs_close(f) != -1);
+        c = tfs_close(f);
+        if (c != -1) {
+            --open_files;
+        }
+        assert(c != -1);
+        printf("close: f:%d c:%d open_files:%d\n", f, c, open_files);
+        //assert(tfs_close(f) != -1);
     }
     return NULL;
 }
@@ -48,15 +55,13 @@ int main() {
 
     assert(tfs_init() != -1);
 
-    pthread_t t;
-    
-    assert(f != -1);
+    pthread_t t[2];
 
-    assert(pthread_create(&t, NULL, fn_thread, NULL) == 0);
+    assert(pthread_create(&t[0], NULL, fn_thread, NULL) == 0);
+    assert(pthread_create(&t[1], NULL, fn_thread, NULL) == 0);
     assert(tfs_destroy_after_all_closed() != -1);
-    printf("Tfs_destroy\n");
+    printf("Tfs_destroy %d\n", open_files);
     assert(open_files == 0);
-    pthread_join(t, NULL);
     // No need to join thread
     printf("Successful test.\n");
 
